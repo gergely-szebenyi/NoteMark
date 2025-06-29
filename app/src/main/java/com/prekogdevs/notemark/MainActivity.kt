785f4a188c6c881ffcbe6dcf0cace7ae6980bad2
gergely-szebenyi <gergelyszebenyi@gmail.com>
2025-06-29 17:25:27 +0200

    Splash screen API12+

--- app/src/main/java/com/prekogdevs/notemark/MainActivity.kt
+++ app/src/main/java/com/prekogdevs/notemark/MainActivity.kt
@@ -11,37 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.prekogdevs.notemark.ui.theme.NoteMarkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NoteMarkTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    Text(
+                        modifier = Modifier.padding(innerPadding),
+                        text = "Android"
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteMarkTheme {
-        Greeting("Android")
     }
 }
